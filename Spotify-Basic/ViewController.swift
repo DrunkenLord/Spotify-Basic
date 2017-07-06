f200ded8edefdb0027c302c178a0d9a6087b1adf
Mehul Singhal <mehulbtp@gmail.com>
2017-07-06 23:54:13 +0530

    Basic spotify API use. For getting data back do run it by using it on VPN enabled network

--- Spotify-Basic/ViewController.swift
+++ Spotify-Basic/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
+    
+    typealias JSONstandards = [String: AnyObject]
+    
+    var searchURL = "https://api.spotify.com/v1/search?q=Shawn+Mendes&type=track"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        callAlamo(url: searchURL)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func callAlamo(url: String)
+    {
+        Alamofire.request(url).response(completionHandler:{
+            
+            response in
+            
+            self.parseData(JSONdata: response.data!)
+        
+        })
+    }
+    
+    
+    func parseData (JSONdata: Data)
+    {
+    
+        do{
+            var readAbleJSON = try JSONSerialization.jsonObject(with: JSONdata, options: .mutableContainers) as? JSONstandards
+            print(readAbleJSON)
+        }
+        catch{
+            print(error)
+        }
     }
 
 
