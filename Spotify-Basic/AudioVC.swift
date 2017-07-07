4d4ae02e731df99014ff65d4c315aaf727581747
Mehul Singhal <mehulbtp@gmail.com>
2017-07-08 00:29:41 +0530

    Complete application with album art, music play pause and searching song of your favourite artist features. More features on the way.

--- Spotify-Basic/AudioVC.swift
+++ Spotify-Basic/AudioVC.swift
@@ -8,11 +8,15 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+
 class AudioVC : UIViewController {
     
     var image = UIImage()
     var mainSongTitle = String()
+    var mainSongURL = String()
     
+    @IBOutlet weak var playpausebtn: UIButton!
     @IBOutlet weak var background: UIImageView!
     
     @IBOutlet weak var mainImageView: UIImageView!
@@ -26,6 +30,73 @@ class AudioVC : UIViewController {
         background.image = image
         mainImageView.image = image
         
+        if mainSongURL == "sdfjklasdjlf"
+        {
+            playpausebtn.setTitle("NA", for: .normal)
+        }
+        else
+        {
+            
+            downloadFileFromURl(url: URL(string: mainSongURL)!)
+            
+            playpausebtn.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playpausebtn.imageView?.clipsToBounds = true
+            playpausebtn.imageView?.layer.cornerRadius = 32
+        }
+    }
+        
+    
+    func downloadFileFromURl(url: URL)
+    {
+        var downloadTask = URLSessionDownloadTask()
+        downloadTask = URLSession.shared.downloadTask(with: url, completionHandler: {
+            
+            customURL, response, error in
+            
+            self.play(url: customURL!)
+            
+        })
+        downloadTask.resume()
+
+    }
+    
+    
+    
+    func play(url: URL)
+    {
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            player.play()
+        }
+        catch{
+            print(error)
+        }
+    
+    }
+    
+    @IBAction func playpausebtnaction(_ sender: Any) {
+        
+        if player.isPlaying
+        {
+            player.pause()
+            playpausebtn.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            playpausebtn.imageView?.clipsToBounds = true
+            playpausebtn.imageView?.layer.cornerRadius = 32
+        }
+        else
+        {
+            player.play()
+            playpausebtn.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playpausebtn.imageView?.clipsToBounds = true
+            playpausebtn.imageView?.layer.cornerRadius = 32
+        }
+        
+    }
+    
+    override var prefersStatusBarHidden: Bool
+        {
+        return true
     }
     
     
--- Spotify-Basic/ListViewController.swift
+++ Spotify-Basic/ListViewController.swift
@@ -8,21 +8,48 @@
 
 import UIKit
 import Alamofire
+import AVFoundation
+
+
+var player = AVAudioPlayer()
+
+var holderOfurl : String!
 
 struct post {
     let image : UIImage!
     let name : String!
 }
 
+struct urlholder
+{
+    let previewURL : String!
+}
+
 var albumArt : UIImage!
 
-class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UITableView!
+    
+    var searchURL = String()
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let keywords = searchBar.text
+        let finalKeywords = keywords?.replacingOccurrences(of: " ", with: "+")
+        searchURL = "https://api.spotify.com/v1/search?q=\(finalKeywords!)"
+        callAlamo(url: searchURL)
+        tableView.reloadData()
+        self.view.endEditing(true)
+
+    }
     
     var posts = [post]()
     
+    var green = [urlholder]()
+    
     typealias JSONstandards = [String: AnyObject]
     
-    var searchURL = "https://api.spotify.com/v1/search?"
+    
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -33,8 +60,7 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
-        callAlamo(url: searchURL)
-    }
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -44,8 +70,8 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     func callAlamo(url: String)
     {
         
-        let parameters = ["q":"Shawn Mendes","type":"track"]
-        let headers = ["Accept":"application/json", "Authorization":"Bearer BQA-I5IDq7_i50Jpz5qp8eMXqBoUKBFgNPI_B0Xr_F25TpHRlfn0c65IL_G63tNnfZIMELvdyzRv1SYuX9PasJRkJFqeTOZPfl923NCm1SuFrPB_xbk1UnT32YVc4M8_3Ud-4YKmWPBtVZMHpFVPA5l35cKhvgX9D1X7iXW15W0-Zokmvgqu5dXvDkRLPYtNAEXuH1UFwSo7YeFrGI8k4BbK5mnzwB_s-Q6fw72w8anFxFceoyWOMoO49BWo7NyRm6rdZaWSm3oPmqatn1TIw0P1rhQ1HKYzDyyOK5sEJUxFGoabQIRA4wCrbCbpB_kfp1o" ]
+        let parameters = ["type":"track"]
+        let headers = ["Accept":"application/json", "Authorization":"Bearer BQCGU8fdkpF154bWFKjuBGejDHK6icK7delaPZHlNKMG-cnv-jIoSLkmpKVHQFLac-agyP7SOyT3Ye8z2X6nP0Oy6piHyroILRcmktMJy4kUsEMW1HC8AF5Jbrg9HYcySVYKTGk1EoWEhtyDy8dVHCPUJkposx6VYinQhp9HiTPYpcvl6nXS8SrGKeLfnl7uIyiSLkscdCPiB8QhmUrW469X_UU0BsyB1N54UOimVs7Mde4xVCC9OeXpSqdGVQm2F8E-IgMcBwMXloL_NsnjUevZgRo_XLIBfQzKuFyMM0YIV6_8FTXcC11wkdIoXI8gzm8" ]
         Alamofire.request(url, method: .get, parameters: parameters, headers: headers).response(completionHandler:{
             
             response in
@@ -61,12 +87,32 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         do{
             var readAbleJSON = try JSONSerialization.jsonObject(with: JSONdata, options: .mutableContainers) as! JSONstandards
-            
+            //print(readAbleJSON)
+            if let expiredToken = readAbleJSON["error"] as? JSONstandards
+            {
+                let optionMenu = UIAlertController(title: nil, message: "The Token has been expired. Contact developer.", preferredStyle: .actionSheet)
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                optionMenu.addAction(cancelAction)
+                present(optionMenu, animated: true, completion: nil)
+            }
+            else{
             if let tracks =  readAbleJSON["tracks"] as? JSONstandards{
                 if let items = tracks["items"] as? [JSONstandards]{
                     for i in 0..<items.count{
                         let item = items[i]
+                        
                         let name = item["name"] as! String
+                        if let urlTemp = item["preview_url"] as? String
+                        {
+                           
+                            holderOfurl = item["preview_url"] as! String!
+                        }
+                        else
+                        {
+                            holderOfurl = "sdfjklasdjlf"
+                            
+                        }
+
                         if let album = item["album"] as? JSONstandards{
                             
                             if let images = album["images"] as? [JSONstandards]{
@@ -81,9 +127,10 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
                             }
                         }
                         posts.append(post.init(image: albumArt, name: name))
-                        
+                        green.append(urlholder.init(previewURL: holderOfurl))
                         print(posts[0])
                         self.tableView.reloadData()
+                    }
                     }
                     
                 }
@@ -114,6 +161,8 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         let mainImageView = cell?.viewWithTag(2) as! UIImageView
         mainImageView.image = posts[indexPath.row].image
+        mainImageView.clipsToBounds = true
+        mainImageView.layer.cornerRadius = 45
         //print(posts[indexPath.row].image)
         let mainLabel = cell?.viewWithTag(1) as! UILabel
         mainLabel.text = posts[indexPath.row].name
@@ -133,6 +182,7 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         vc.image = posts[indexPath!].image
         vc.mainSongTitle = posts[indexPath!].name
+        vc.mainSongURL = green[indexPath!].previewURL
     }
     
     override var prefersStatusBarHidden: Bool
